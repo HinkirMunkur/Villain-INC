8ab0a559f9721f4ec9c9346edc036f9fb57db55e
mfkucuk <mfkucuk555@gmail.com>
2023-05-28 03:59:57 +0300

    Update FeetTriggerHandler.cs

--- VillainINC/Assets/Scripts/SubjectBasic/SubjectTrıggerHandler/FeetTriggerHandler.cs
+++ VillainINC/Assets/Scripts/SubjectBasic/SubjectTrıggerHandler/FeetTriggerHandler.cs
@@ -19,7 +19,6 @@ public class FeetTriggerHandler : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D col) 
     {
-        subjectBasic.MovementBehaviour.IsInGround = false;
         subjectBasic.MovementBehaviour.IsInAir = true;
         StartCoroutine(subjectBasic.MovementBehaviour.FindFallSpeed());
 
