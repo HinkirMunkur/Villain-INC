a4df3def8f593c73d2be72b4e8d6dc9265a71774
mfkucuk <mfkucuk555@gmail.com>
2023-06-01 03:30:32 +0300

    Puzzle music

--- VillainINC/Assets/Scripts/UI/GameSceneButtonActivity/StartGameSceneButtonActivity.cs
+++ VillainINC/Assets/Scripts/UI/GameSceneButtonActivity/StartGameSceneButtonActivity.cs
@@ -1,8 +1,9 @@
-
+using Munkur;
 public class StartGameSceneButtonActivity : GameSceneButtonActivity
 {
     protected override void Clicked()
     {
+        AudioManager.Instance.StopMusic();
         LevelController.Instance.LoadLevelWithIndex(LevelController.Instance.GetCurrentSavedLevelIndex());
     }
 }
