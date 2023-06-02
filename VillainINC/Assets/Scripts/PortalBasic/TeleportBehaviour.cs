0ebf72955fa4ea5770c7af53aaf38fd09dd8e7a5
borakaraa <kborakaraa@gmail.com>
2023-06-03 02:18:01 +0300

    Sound Effect Added

--- VillainINC/Assets/Scripts/PortalBasic/TeleportBehaviour.cs
+++ VillainINC/Assets/Scripts/PortalBasic/TeleportBehaviour.cs
@@ -5,12 +5,10 @@ public class TeleportBehaviour : MonoBehaviour
 {
     [SerializeField] private PortalBasic _portalBasic;
     [SerializeField] private float _pushSpeed;
-    [SerializeField] private ParticleSystem _littleParticles;
-    
+
     public void Teleport(Collider2D col) 
     {
         AudioManager.Instance.PlaySoundEffect("Portal");
-        _littleParticles.Play(true);
         col.transform.position = _portalBasic.OtherPortal.TeleportPoint.position;
 
         if (!col.CompareTag("Player")) 
