e2c811eafb71e5e3a4cfa89d2fd7380fe57a092a
mfkucuk <mfkucuk555@gmail.com>
2023-06-04 17:24:47 +0300

    back to menu

---
+++ VillainINC/Assets/Scripts/UI/ImageActivity/BackToMenuImageActivity.cs
@@ -0,0 +1,22 @@
+using Munkur;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BackToMenuImageActivity : ImageActivity
+{
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        if(LevelFlowManager.Instance.IsLevelFinished)
+        {
+            return;
+        }
+
+        if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        
+        AudioManager.Instance.PlaySoundEffect("Back");
+        TransitionManager.Instance.EndSceneTransition(LevelController.Instance.GetSceneNameWithIndex(1));
+    }
+}
